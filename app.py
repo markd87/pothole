ec6a1daccdf045416668318a48625814f26846a1
Mark Danovich <mark.danovich@gmail.com>
2020-02-16 23:46:16 +0000

    reduced import, add me

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 from PIL import Image as img
-from fastai.vision import *
+from fastai.vision import load_learner, open_image
 
 st.title("Pothole identifier")
+st.markdown("by Mark Danovich, mark.danovich@gmail.com")
 
 uploaded_file = st.file_uploader("Choose an image...", type=["jpeg","jpg"])
 if uploaded_file is not None:
@@ -12,4 +13,8 @@ if uploaded_file is not None:
     st.write("Classifying...")
     learn = load_learner(".", file="model.pkl")
     label = learn.predict(open_image(uploaded_file))
-    st.write(f"{str(label[0])}, Probability: {label[2].max():.3f}")
+    if str(label[0]) == 'pothole':
+        output = 'Pothole'
+    else:
+        output = 'Not a Pothole'
+    st.write(f"{output}, Probability: {label[2].max():.3f}")
