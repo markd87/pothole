0db1165c3c311ec196da84a5c616e663f5696799
Mark Danovich <mark.danovich@gmail.com>
2020-02-19 22:19:48 +0000

    updated req, app

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 import streamlit as st
 from PIL import Image as img
-from fastai.vision import load_learner, open_image
+from fastai.vision import open_image, load_learner, Learner
+# from fastai import defaults
+import torch
+import fastai
+
+fastai.torch_core.defaults.device = torch.device('cpu')
 
 st.title("Pothole identifier")
 st.markdown("by Mark Danovich, mark.danovich@gmail.com")
+
+def get_model() -> Learner:
+    return  load_learner(".", file="model.pkl")
 
 uploaded_file = st.file_uploader("Choose an image...", type=["jpeg","jpg", "png"])
 if uploaded_file is not None:
@@ -11,10 +19,11 @@ if uploaded_file is not None:
     st.image(image, caption='Uploaded Image.', use_column_width=True)
     st.write("")
     st.write("Classifying...")
-    learn = load_learner(".", file="model.pkl")
+    learn = get_model()
     label = learn.predict(open_image(uploaded_file))
     if str(label[0]) == 'pothole':
         output = 'Pothole'
     else:
         output = 'Not a Pothole'
     st.write(f"{output}, Probability: {label[2].max():.3f}")
+
